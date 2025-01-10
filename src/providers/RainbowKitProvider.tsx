12c095ed0e8e27969a1052707ff653b7921f0197
codetrexlenovo <codetrexlenovo01@gmail.com>
2025-01-10 19:15:49 +0530

    remove auth

--- src/providers/RainbowKitProvider.tsx
+++ src/providers/RainbowKitProvider.tsx
@@ -6,18 +6,18 @@ import "@rainbow-me/rainbowkit/styles.css";
 import { State, WagmiProvider } from "wagmi";
 import {
   RainbowKitProvider as NextRainbowKitProvider,
-  RainbowKitAuthenticationProvider,
+  // RainbowKitAuthenticationProvider,
   darkTheme,
 } from "@rainbow-me/rainbowkit";
-import { ReactNode, useState } from "react";
+import { ReactNode } from "react";
 import ReactQueryProvider from "./ReactQueryProvider";
 import wagmiConfig from "@/lib/config/wagmi";
-import { authenticationAdapter } from "@/lib/utils/authenticationAdapter";
-import useAsyncEffect from "@/lib/hooks/useAsyncEffect";
-import { isAuthAction } from "@/lib/actions/auth";
-import { Optional } from "@/lib/types/common";
-import { eventEmitter } from "@/lib/config/eventEmitter";
-import { EMITTER_EVENTS } from "@/lib/constants";
+// import { authenticationAdapter } from "@/lib/utils/authenticationAdapter";
+// import useAsyncEffect from "@/lib/hooks/useAsyncEffect";
+// import { isAuthAction } from "@/lib/actions/auth";
+// import { Optional } from "@/lib/types/common";
+// import { eventEmitter } from "@/lib/config/eventEmitter";
+// import { EMITTER_EVENTS } from "@/lib/constants";
 // import { bsc } from "viem/chains";
 type RainbowKitProviderProps = {
   children: ReactNode;
@@ -28,28 +28,28 @@ export default function RainbowKitProvider({
   children,
   initialState,
 }: RainbowKitProviderProps) {
-  const [isLoading, setIsLoading] = useState(true);
-  const [isAuth, setIsAuth] = useState<Optional<boolean>>();
-  useAsyncEffect(async () => {
-    const { isAuth } = await isAuthAction();
+  // const [isLoading, setIsLoading] = useState(true);
+  // const [isAuth, setIsAuth] = useState<Optional<boolean>>();
+  // useAsyncEffect(async () => {
+  //   const { isAuth } = await isAuthAction();
 
-    setIsAuth(isAuth);
-    setIsLoading(false);
+  //   setIsAuth(isAuth);
+  //   setIsLoading(false);
 
-    eventEmitter.on(EMITTER_EVENTS.SIGN_IN, () => setIsAuth(true));
+  //   eventEmitter.on(EMITTER_EVENTS.SIGN_IN, () => setIsAuth(true));
 
-    eventEmitter.on(EMITTER_EVENTS.SIGN_OUT, () => setIsAuth(false));
+  //   eventEmitter.on(EMITTER_EVENTS.SIGN_OUT, () => setIsAuth(false));
 
-    return () => {
-      eventEmitter.removeListener(EMITTER_EVENTS.SIGN_IN);
-    };
-  }, []);
+  //   return () => {
+  //     eventEmitter.removeListener(EMITTER_EVENTS.SIGN_IN);
+  //   };
+  // }, []);
 
-  const status = isLoading
-    ? "loading"
-    : isAuth
-    ? "authenticated"
-    : "unauthenticated";
+  // const status = isLoading
+  //   ? "loading"
+  //   : isAuth
+  //   ? "authenticated"
+  //   : "unauthenticated";
 
   return (
     <WagmiProvider
@@ -57,17 +57,17 @@ export default function RainbowKitProvider({
       initialState={initialState}
     >
       <ReactQueryProvider>
-        <RainbowKitAuthenticationProvider
+        {/* <RainbowKitAuthenticationProvider
           adapter={authenticationAdapter}
           status={status}
-        >
+        > */}
           <NextRainbowKitProvider
             theme={darkTheme()}
             modalSize='compact'
           >
             {children}
           </NextRainbowKitProvider>
-        </RainbowKitAuthenticationProvider>
+        {/* </RainbowKitAuthenticationProvider> */}
       </ReactQueryProvider>
     </WagmiProvider>
   );
