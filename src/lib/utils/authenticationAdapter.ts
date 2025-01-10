f0caefd282e5707e12cf9631e18226b6c88f8860
codetrexlenovo <codetrexlenovo01@gmail.com>
2025-01-10 19:20:52 +0530

    remove page auth

--- src/lib/utils/authenticationAdapter.ts
+++ src/lib/utils/authenticationAdapter.ts
@@ -1,52 +1,52 @@
-import { createSiweMessage } from 'viem/siwe';
-import { eventEmitter } from "../config/eventEmitter";
-import { EMITTER_EVENTS } from "../constants";
-import { signInAction } from "../actions/auth";
-import { createAuthenticationAdapter } from "@rainbow-me/rainbowkit";
+// import { createSiweMessage } from 'viem/siwe';
+// import { eventEmitter } from "../config/eventEmitter";
+// import { EMITTER_EVENTS } from "../constants";
+// import { signInAction } from "../actions/auth";
+// import { createAuthenticationAdapter } from "@rainbow-me/rainbowkit";
 
-export const authenticationAdapter = createAuthenticationAdapter({
-  getNonce: async () => {
-    const response = await fetch('/api/nonce');
-    const data: { nonce: string; } = await response.json();
+// export const authenticationAdapter = createAuthenticationAdapter({
+//   getNonce: async () => {
+//     const response = await fetch('/api/nonce');
+//     const data: { nonce: string; } = await response.json();
 
-    return new Promise((resolve) => resolve(data.nonce));
-  },
-  createMessage: ({ nonce, chainId }) => {
-    return createSiweMessage({
-      domain: window.location.host,
-      //address,
-      statement: 'Sign in with Ethereum to the app.',
-      uri: window.location.origin,
-      version: '1',
-      chainId,
-      nonce,
-      address: '0x'
-    });
-  },
-  verify: async ({ message, signature }) => {
-    const verifyRes = await fetch(`${process.env.NEXT_PUBLIC_TOKEN_SALE_URL}/api/verify`, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ message, signature }),
-    });
+//     return new Promise((resolve) => resolve(data.nonce));
+//   },
+//   createMessage: ({ nonce, chainId }) => {
+//     return createSiweMessage({
+//       domain: window.location.host,
+//       //address,
+//       statement: 'Sign in with Ethereum to the app.',
+//       uri: window.location.origin,
+//       version: '1',
+//       chainId,
+//       nonce,
+//       address: '0x'
+//     });
+//   },
+//   verify: async ({ message, signature }) => {
+//     const verifyRes = await fetch(`${process.env.NEXT_PUBLIC_TOKEN_SALE_URL}/api/verify`, {
+//       method: 'POST',
+//       headers: { 'Content-Type': 'application/json' },
+//       body: JSON.stringify({ message, signature }),
+//     });
 
-    if (!verifyRes.ok) {
-      throw new Error("Failed to verify signature");
-    }
-    const data = await verifyRes.json();
-    await signInAction({ jwt: data.jwt });
+//     if (!verifyRes.ok) {
+//       throw new Error("Failed to verify signature");
+//     }
+//     const data = await verifyRes.json();
+//     await signInAction({ jwt: data.jwt });
 
-    eventEmitter.emit(EMITTER_EVENTS.SIGN_IN);
+//     eventEmitter.emit(EMITTER_EVENTS.SIGN_IN);
 
-    return true;
-  },
-  signOut: async () => {
-    await fetch('/api/logout');
-  },
-  getMessageBody: (args: { message: string }): string => {
-    if (!args.message) {
-      throw new Error('Message is required to generate the body');
-    }
-    return JSON.stringify(args.message); 
-  }
-});
+//     return true;
+//   },
+//   signOut: async () => {
+//     await fetch('/api/logout');
+//   },
+//   getMessageBody: (args: { message: string }): string => {
+//     if (!args.message) {
+//       throw new Error('Message is required to generate the body');
+//     }
+//     return JSON.stringify(args.message); 
+//   }
+// });
